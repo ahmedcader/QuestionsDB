01f35d3515b4fa368ced21caa1327cba5a307b51
Ahmed Cader <ahmedcader@live.com>
2017-04-29 22:46:42 +1200

    refactored urls.py for certain apps. navigation should be much more straightforward and smoother.

--- dev/src/QuestionsDB/category/views.py
+++ dev/src/QuestionsDB/category/views.py
@@ -5,11 +5,8 @@ from category.models import Category
 def category_page(request, category_slug):
     parent = request.GET.get('p')
     child = request.GET.get('c')
+    print(category_slug)
     category = Category.objects.get(id=parent, sub_category_id=child)
-    if parent is not None and child is not None:
-        sub_categories = Category.objects.filter(sub_category_id=parent)
-    else:
-        sub_categories = Category.objects.filter(sub_category__name=category_slug)
-    context = {'sub_categories': sub_categories, 'category': category, }
+    sub_categories = Category.objects.filter(sub_category_id=parent)
+    context = {'category': category, 'sub_categories': sub_categories, }
     return render(request, 'category.html', context)
-    # return render(request, 'category.html')
