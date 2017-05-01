549093a3563a6b3a26618084ae5b2891835d3601
Ahmed Cader <ahmedcader@live.com>
2017-05-01 23:47:18 +1200

    comments now successfully collapse and show accordingly and have the ability to view the comment model. Functionality is yet to be implemented.

--- dev/src/QuestionsDB/static/js/main.js
+++ dev/src/QuestionsDB/static/js/main.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
-    $("a.comment-hide").click(function () {
-        // console.log($(this).parent().parent().children().toggle())
-        $(this).getElementsByClassName('div.comment-container').next().toggle()
+    $("a.comment-reply").click(function (e) {
+        e.preventDefault();
+        $(this).parent().parent().children('div.add-comment.add-reply').slideToggle();
+        var text = $(this).text() === 'reply' ? 'cancel reply' : 'reply';
+        $(this).text(text)
+    });
+
+    $("a.comment-hide").click(function (e) {
+        e.preventDefault();
+        $(this).parent().parent().next().slideToggle();
+         var text = $(this).text() === 'hide replies' ? 'show replies' : 'hide replies';
+        $(this).text(text)
     });
 });
