80e2039019708fe0369a98f9187ee7d254792f1f
Ahmed Cader <ahmedcader@live.com>
2017-05-03 00:57:01 +1200

    Comments now display how many children and replies it has.

--- dev/src/QuestionsDB/posts/admin.py
+++ dev/src/QuestionsDB/posts/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from posts.models import Post
+from comment.models import Comment
 from django import forms
 
-class PostAdmin(admin.ModelAdmin):
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', "user", "post",'comment']
+    list_filter = ['id', "user", "post",]
+    search_fields = ['id', "user", "post",]
 
+    class Meta:
+        model = Comment
+
+class PostAdmin(admin.ModelAdmin):
     list_display = ['title', "id", "user", 'parent', 'slug_field']
     list_filter = ['title', "id", "user", 'parent']
     search_fields = ['title', "id", "user", 'parent']
@@ -13,3 +21,4 @@ class PostAdmin(admin.ModelAdmin):
         model = Post
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
