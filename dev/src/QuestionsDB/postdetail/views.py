2e72c87ef65dae262d305ecca678f4f5f32aeeab
CDemha <AhmedCader@Live.com>
2017-06-03 18:51:19 +1200

    comments can be added. So can replies.

--- dev/src/QuestionsDB/postdetail/views.py
+++ dev/src/QuestionsDB/postdetail/views.py
@@ -5,13 +5,24 @@ from comment.models import Comment
 from datetime import datetime
 from django.core.urlresolvers import reverse
 from home import views
+from django.http.response import HttpResponseRedirect
 
 
 def post_detail_page(request, parent_id, parent_category_slug, question_id):
     post = Post.objects.get(id=question_id)
     comments = Comment.objects.filter(post=post)
     categories = Category.objects.get(id=parent_id)
-    context = {'post': post, 'comments': comments,}
+    context = {'post': post, 'comments': comments, }
     user_comment = request.POST.get('user-comment')
-    print(user_comment)
+    if request.POST:
+        comment_parent = request.POST.get('reply-id')
+        comment_obj = Comment(
+            user=request.user,
+            post=post,
+            comment=user_comment,
+            date_posted=datetime.now(),
+            date_edited=datetime.now(),
+            parent=Comment.objects.filter(id=comment_parent).first())
+        comment_obj.save()
+        return HttpResponseRedirect(request.path)
     return render(request, 'post_detail.html', context)
